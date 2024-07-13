41610e1a813de0277e21868914a23fc56333583b
Chris Speciale <christopher.gaf@gmail.com>
2024-07-13 05:50:06 -0400

    Animations!

--- src/bf/graphics/Animation.hx
+++ src/bf/graphics/Animation.hx
@@ -1,9 +1,12 @@
 package bf.graphics;
 
-import openfl.display.MovieClip;
+import starling.textures.Texture;
+import starling.display.MovieClip;
 
 class Animation extends MovieClip {
-    public function new(){
-        super();
-    }
+
+    public function new(textures:Array<Texture>, fps:Int)
+        {
+            super(textures, fps);
+        }
 }
